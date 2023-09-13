91cd997e7914613dd0ea04d7bee0b108abe86e19
Severine Candelier <severine.candelier@gmail.com>
2023-09-13 19:41:06 +0200

    Module structure

---
+++ ModuleInstaller.php
@@ -0,0 +1,8 @@
+<?php
+namespace ICM\ModuleInstaller;
+
+class ModuleInstaller extends \ExternalModules\AbstractExternalModule {
+	public function __construct() {
+		parent::__construct();
+		// Other code to run when object is instantiated
+	}}
