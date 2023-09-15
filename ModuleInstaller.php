5a3aef7525a4a4ee8567e5eeda0fa9639c141012
scandelier <severine.candelier@icm-institute.org>
2023-09-15 17:02:49 +0200

    Functional (install an uploaded zip) but not fancy

--- ModuleInstaller.php
+++ ModuleInstaller.php
@@ -1,8 +1,152 @@
 <?php
-namespace ICM\ModuleInstaller;
 
-class ModuleInstaller extends \ExternalModules\AbstractExternalModule {
-	public function __construct() {
-		parent::__construct();
-		// Other code to run when object is instantiated
-	}}
+namespace ICM\ModuleInstaller;
+include 'utils/functions.php';
+
+define('APP_PATH_EXTERNAL_MODULES', dirname(APP_PATH_DOCROOT) . DIRECTORY_SEPARATOR . 'modules');
+define('TEMP_FOLDER', APP_PATH_TEMP . DIRECTORY_SEPARATOR . 'module_installer');
+
+class ModuleInformation
+{
+    public string $parent_folder_name;
+    public string $name = "";
+    public string $version = "";
+    public string $path = "";
+}
+
+
+class ModuleInstaller extends \ExternalModules\AbstractExternalModule
+{
+    public function __construct()
+    {
+        parent::__construct();
+
+        if (file_exists(TEMP_FOLDER) && !is_dir(TEMP_FOLDER)) {
+            throw new \Exception("The temporary folder is not a directory.");
+        }
+        if (!file_exists(TEMP_FOLDER)) {
+            // Create the temporary folder
+            if (!mkdir(TEMP_FOLDER, 0777, true)) {
+                throw new \Exception("The temporary folder could not be created.");
+            }
+        }
+    }
+
+    /**
+     * Moves the file to a temporary location and returns and filename.
+     *
+     * @param $uploaded_file
+     * @return string
+     * @throws \Exception
+     */
+    public function moveZipFileToTempLocation($filename): string
+    {
+        // Create a temporary filename
+        $temp_filename = bin2hex(random_bytes(16)) . '.zip';
+
+        // Move the uploaded file to the temporary location
+        if (!move_uploaded_file($filename, TEMP_FOLDER . '/' . $temp_filename)) {
+            throw new \Exception("The uploaded file could not be moved to a temporary location.");
+        }
+        return $temp_filename;
+    }
+
+    /**
+     * Deletes the temporary ZIP file.
+     *
+     * @param $temp_filename
+     */
+    public function deleteTempZipFile($temp_filename)
+    {
+        unlink(TEMP_FOLDER . '/' . $temp_filename);
+    }
+
+    public function getModuleInformationFromZip($tmp_filename): ModuleInformation
+    {
+        $module_info = new ModuleInformation();
+
+        try {
+            // Extract the ZIP in place
+            $zip = new \ZipArchive;
+            if ($zip->open(TEMP_FOLDER . '/' . $tmp_filename) === TRUE) {
+                // Get the parent folder name
+                $module_info->parent_folder_name = rtrim($zip->getNameIndex(0), '/');
+                // Extract the ZIP file in place
+                $zip->extractTo(TEMP_FOLDER);
+                $zip->close();
+            } else {
+                throw new \Exception("The ZIP file could not be opened.");
+            }
+
+            // Check that the module contains a config.json file and read it
+            if (!file_exists(TEMP_FOLDER . '/' . $module_info->parent_folder_name . '/config.json')) {
+                throw new \Exception("The ZIP file does not contain a config.json file.");
+            }
+
+            // Get the module name from the config.json file
+            $config = json_decode(file_get_contents(TEMP_FOLDER . '/' . $module_info->parent_folder_name . '/config.json'), true);
+
+            if (!isset($config['name'])) {
+                throw new \Exception("The config.json file does not contain a name.");
+            }
+            $module_info->name = $config['name'];
+
+            // Extract the version number (x.y.z) from the parent folder name with a regex
+            if (preg_match('/-([0-9]+)\.([0-9]+)\.([0-9]+)$/', $module_info->parent_folder_name, $matches)) {
+                $module_info->version = substr($matches[0], 1);
+            }
+
+            // Get the module path from the parent folder name
+            $path_from_name = $module_info->version ? substr($module_info->parent_folder_name, 0, -strlen($module_info->version) - 1)
+                : $module_info->parent_folder_name;
+            // Convert hyphens and special characters to underscores, and convert to lowercase
+            $module_info->path = strtolower(preg_replace('/[^a-zA-Z0-9]/', '_', $path_from_name));
+
+        }
+        catch (\Exception $e) {
+            throw new \Exception($e->getMessage());
+        } finally {
+            delete_directory(TEMP_FOLDER . '/' . $module_info->parent_folder_name);
+        }
+
+        return $module_info;
+    }
+
+    public function installModuleFromZip($tmp_filename, $module_info)
+    {
+        $module_path = $module_info->path . '_v' . $module_info->version;
+
+        // TODO: allow? With a warning?
+        // Check if the module already exists
+        if (file_exists(APP_PATH_EXTERNAL_MODULES . '/' . $module_path)) {
+            throw new \Exception("The module already exists in $module_path.");
+        }
+
+        try {
+            // Rename the parent folder of the zip to the module path
+            $zip = new \ZipArchive;
+            if ($zip->open(TEMP_FOLDER . DIRECTORY_SEPARATOR . $tmp_filename) !== TRUE) {
+                throw new \Exception("The ZIP file could not be opened.");
+            }
+            $i = 0;
+            while ($item_name = $zip->getNameIndex($i)){
+                $item_name_end = substr($item_name, strpos($item_name, "/"));
+                $zip->renameIndex($i++, $module_path . DIRECTORY_SEPARATOR . $item_name_end);
+            }
+            $zip->close();
+
+            // Now extract the zip to the modules folder
+            $zip = new \ZipArchive;
+            if ($zip->open(TEMP_FOLDER . DIRECTORY_SEPARATOR . $tmp_filename) === TRUE) {
+                $zip->extractTo(APP_PATH_EXTERNAL_MODULES);
+                $zip->close();
+            } else {
+                throw new \Exception("The ZIP file could not be opened.");
+            }
+        }
+        catch (\Exception $e) {
+            throw new \Exception($e->getMessage());
+        }
+    }
+
+}
--- pages/index.php
+++ pages/index.php
@@ -1,3 +1,64 @@
 <?php
+global $module;
 
-echo "Hello world!";
+// TODO: Check if the current user is an admin.
+if (!SUPER_USER) { // Not good, we want to test if the current user is an admin. SUPER_USER is not the same as admin.
+    echo RCView::errorBox("You don't have permission to access this page.");
+    exit();
+}
+
+// Form errors, by field name
+$form_errors = array();
+
+// Form handling
+if (isset($_POST['submit'])) {
+    // Validate that a file was uploaded
+    if (empty($_FILES['module_zip']['name'])) {
+        $form_errors['module_zip'] = "No file was uploaded.";
+        // Validate that the file is a ZIP file
+    } else if (pathinfo($_FILES['module_zip']['name'], PATHINFO_EXTENSION) != 'zip') {
+        $form_errors['module_zip'] = "The uploaded file is not a ZIP file.";
+    }
+
+    $tmp_filename = $module->moveZipFileToTempLocation($_FILES['module_zip']['tmp_name']);
+
+    // Get module information
+    $module_info = $module->getModuleInformationFromZip($tmp_filename);
+
+    echo RCView::warnBox("Module name: $module_info->name, version: $module_info->version, path: $module_info->path");
+
+    // Install the module
+    $success = $module->installModuleFromZip($tmp_filename, $module_info);
+
+    // Clean temp file
+    $module->deleteTempZipFile($tmp_filename);
+}
+
+
+// HTML Rendering
+
+$title = '<i class="fas fa-cube"></i> ' . REDCap::escapeHtml('External Module Installer');
+echo RCView::h3([], $title);
+
+// HTML form creation
+$form_content = "";
+
+$form_content .= RCView::label(['for' => 'module_name'], 'Module name ')
+               . RCView::input(['type' => 'text', 'name' => 'module_name', 'id' => 'module_name', 'style' => 'margin-bottom: 10px;'])
+               . RCView::br();
+if (isset($form_errors['module_name'])) {
+    $form_content .= RCView::div(['class' => 'alert alert-danger p-1'], $form_errors['module_name']);
+}
+
+$form_content .= RCView::label(['for' => 'module_zip'], 'Module ZIP file ')
+               . RCView::file(['name' => 'module_zip', 'id' => 'module_zip', 'style' => 'margin-bottom: 10px;'])
+               . RCView::br();
+if (isset($form_errors['module_zip'])) {
+    $form_content .= RCView::div(['class' => 'alert alert-danger p-1'], $form_errors['module_zip']);
+}
+
+$form_content .= RCView::button(['class' => 'btn btn-primary', 'type' => 'submit', 'name' => 'submit'], 'Install module');
+
+echo RCView::form(['method' => 'post', 'action' => '', 'enctype' => 'multipart/form-data', 'style' => 'margin-bottom: 20px;'], $form_content);
+
+
